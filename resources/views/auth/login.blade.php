4cef979bd3f91927070ec031b50a7fbdc00ad062
fodjou <michelle.sonpoho@2026.ucac-icam.com>
2024-02-18 16:06:44 +0100

    userauth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Welcome</title>
     <style>
+
         /* Styles CSS */
+
         body {
             margin: 0;
             padding: 0;
         }
+
+
         .background-image {
             background: linear-gradient(178deg, #548F74 0%, #FFD892E0 62%, #FFF2DBF5 73%, #FFFFFF 100%),
             url({{ asset('images/background1.png') }}) no-repeat;
@@ -21,7 +25,9 @@
             justify-content: center;
             align-items: center;
         }
+
         .form-container {
+
             background-color: rgb(255, 255, 255);
             padding: 20px;
             border-radius: 60px 0px 60px 10px;
@@ -31,10 +37,13 @@
             left: 612px; /* Nouvelle position left */
             width: 697px; /* Nouvelle largeur */
             height: 741px; /* Nouvelle hauteur*/
+
         }
+
         h1 {
             margin-top: 0;
         }
+
         img {
             top: 150px;
             left: 791px;
@@ -42,10 +51,12 @@
             height: 188px;
             opacity: 1;
         }
+
         label {
             display: block;
             margin-bottom: 5px;
         }
+
         input[type="text"],
         input[type="password"] {
             top: 590px;
@@ -56,9 +67,8 @@
             margin-bottom: 10px;
             border: 1px solid #707070; /* Nouvelle couleur de bordure */
             border-radius: 36px;
-            font-size: 20px; /* Nouvelle taille de police */
-
         }
+
         button[type="submit"] {
             top: 590px;
             left: 714px;
@@ -71,36 +81,38 @@
             border-radius: 36px;
             cursor: pointer;
         }
+
         button[type="submit"]:hover {
-            background-color: #FFD892E0; /* Nouvelle couleur de fond au survol */
+            background-color: #FF9800; /* Nouvelle couleur de fond au survol */
             top: 590px;
             left: 714px;
             width: 515px;
             height: 71px;
             border-radius: 36px;
         }
+
     </style>
 </head>
 <body>
 <div class="background-image">
 
-    <div class="form-container">
-        <img src="{{ asset('images/logo1.png') }}" alt="Logo">
-        <h1>Connexion</h1>
-        <form action="/" method="POST">
-            @csrf
+<div class="form-container">
+    <img src="{{ asset('images/logo1.png') }}" alt="Logo">
+    <h1>Connexion</h1>
+    <form action="/" method="POST">
+        @csrf
 
-            <label for="name">Nom d'utilisateur:</label>
-            <input type="text" id="name" name="name" required>
+        <label for="name">Nom d'utilisateur:</label>
+        <input type="text" id="name" name="name" required>
 
-            <label for="password">Mot de passe:</label>
-            <input type="password" id="password" name="password" required>
+        <label for="password">Mot de passe:</label>
+        <input type="password" id="password" name="password" required>
 
-            <button type="submit">Connexion</button>
-        </form>
+        <button type="submit">Connexion</button>
+    </form>
 
-        <p>Vous n'avez pas de compte ? <a href="{{ route('register') }}">Inscrivez-vous ici</a></p>
-    </div>
+    <p>Vous n'avez pas de compte ? <a href="{{ route('register') }}">Inscrivez-vous ici</a></p>
+</div>
 
 </div>
 </body>
